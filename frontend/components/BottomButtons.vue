3a3a8d1e8ea1c756d5e20f51ade4327ac8f26c8b
lordfuoco <lordfuoco@gmail.com>
2018-03-28 10:35:07 +0200

    feat: add WIP version of bottom buttons and dispatch the action to load the user profile

---
+++ frontend/components/BottomButtons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bottom-buttons">
+    <v-spacer />
+    <v-btn
+      class="add-button"
+      fab
+      color="red">
+      <v-icon>add</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BottomButtons'
+};
+</script>
+
+<style lang="less">
+.bottom-buttons {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 10;
+    margin: auto;
+    padding: 16px;
+    display: flex;
+
+    .add-button {
+        width: 55px;
+        height: 55px;
+    }
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <section class="container">
+  <v-container
+    fluid
+    fill-height>
     <main-map/>
-  </section>
+    <bottom-buttons />
+  </v-container>
 
 </template>
 
 <script>
 import MainMap from '~/components/MainMap.vue';
+import BottomButtons from '~/components/BottomButtons.vue';
 
 export default {
   components: {
-    MainMap
+    MainMap,
+    BottomButtons
+  },
+  async fetch({store}) {
+    await store.dispatch('user/loadGitHubProfile');
   }
 };
 </script>
