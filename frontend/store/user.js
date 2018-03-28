341dd6ab54891dc4f8b17fe73b7738b30618d7e8
lordfuoco <lordfuoco@gmail.com>
2018-03-28 18:14:41 +0200

    feat: user store module with stub logout action and profile handling

--- frontend/store/user.js
+++ frontend/store/user.js
@@ -2,25 +2,45 @@ import { gitHubUserProfile } from '~/integrations/github/queries';
 import { gitHubGraphQlRequest } from '~/integrations/github/utilities';
 
 export const state = () => ({
-  user: undefined,
-  gitHubProfile: undefined,
-  position: undefined
+  user: null,
+  gitHubProfile: null,
+  savedProfile: null,
+  position: null
 });
 
 export const getters = {
+  getUserProfile: state => {
+    return {...state.gitHubProfile, ...state.savedProfile};
+  },
+  getLoginStatus: state => {
+    const ghp = state.gitHubProfile;
+    return ghp && ghp.name && ghp.avatarUrl;
+  },
   getUserPosition: state => {
-    return state.position;
+    if(state.position){
+      return state.position;
+    }
+    return undefined;
+  },
+  isPositionSet: (state, getters) => {
+    return !!getters.getUserPosition;
   }
 };
 
 export const actions = {
-  async loadGitHubProfile ({commit}) {
+  async loadGitHubProfile({commit}) {
     const gh = gitHubGraphQlRequest();
     const { data } = await this.$axios.post(gh.url, gitHubUserProfile(), gh.options);
     commit('SET_USER_GITHUB_PROFILE', {...data.data.viewer });
   },
   setUserPosition({commit}, position) {
     commit('SET_USER_POSITION', position);
+  },
+  logout({commit}) {
+    commit('SET_USER_GITHUB_PROFILE', null);
+  },
+  saveUserProfile({commit}, profile) {
+    commit('SET_SAVED_PROFILE', { ...profile });
   }
 };
 
@@ -30,6 +50,9 @@ export const mutations = {
   },
   SET_USER_POSITION: (state, position) => {
     state.position = position;
+  },
+  SET_SAVED_PROFILE: (state, profile) => {
+    state.savedProfile = profile;
   }
 };
 
