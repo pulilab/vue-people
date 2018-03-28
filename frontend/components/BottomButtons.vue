0ba6c595250d0e2f87e1132a4eeb282c67b6095a
lordfuoco <lordfuoco@gmail.com>
2018-03-28 18:11:30 +0200

    feat: button buttons now react to the status properly

--- frontend/components/BottomButtons.vue
+++ frontend/components/BottomButtons.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="bottom-buttons">
+
+  <div
+    class="bottom-buttons">
     <v-spacer />
-    <v-btn
-      class="add-button"
-      fab
-      color="red">
-      <v-icon>add</v-icon>
-    </v-btn>
+    <v-slide-y-transition>
+      <v-btn
+        v-show="showGoToAddPerson"
+        class=""
+        fab
+        large
+        color="primary"
+        @click="confirmMarkerPosition">
+        <v-icon>check</v-icon>
+      </v-btn>
+    </v-slide-y-transition>
+    <v-slide-y-transition>
+      <v-btn
+        v-show="showAddMarkerButton"
+        class=""
+        fab
+        large
+        color="red"
+        @click="setAddMode(true)">
+        <v-icon>add</v-icon>
+      </v-btn>
+    </v-slide-y-transition>
   </div>
 </template>
 
 <script>
+import {mapActions, mapGetters} from 'vuex';
 export default {
-    name: 'BottomButtons'
+  name: 'BottomButtons',
+  computed: {
+    ...mapGetters({
+      addMode: 'map/isAddMode',
+      isUserPositionSet: 'user/isPositionSet',
+      isLoggedIn: 'user/getLoginStatus'
+    }),
+    showGoToAddPerson() {
+      return this.addMode && this.isUserPositionSet;
+    },
+    showAddMarkerButton() {
+      return !this.addMode && this.isLoggedIn && !this.isUserPositionSet;
+    }
+  },
+  methods: {
+    ...mapActions({
+      setAddMode: 'map/setAddMode'
+    }),
+    confirmMarkerPosition() {
+      this.setAddMode(false);
+      this.$router.push('/user/');
+    }
+  }
 };
 </script>
 
@@ -26,10 +67,5 @@ export default {
     margin: auto;
     padding: 16px;
     display: flex;
-
-    .add-button {
-        width: 55px;
-        height: 55px;
-    }
 }
 </style>
