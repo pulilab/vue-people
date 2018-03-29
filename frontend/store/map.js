8bd63373a5716481e744f9926a2ac1d43c96fb63
lordfuoco <lordfuoco@gmail.com>
2018-03-29 15:53:25 +0200

    feat: added map setter, getter and states for map zoom and center handling

--- frontend/store/map.js
+++ frontend/store/map.js
@@ -1,22 +1,44 @@
+import { root } from "postcss";
+
 export const state = () => ({
-  addMode: false
+  addMode: false,
+  zoom: 13,
+  center: null
 });
 
 export const getters = {
   isAddMode: state => {
     return state.addMode;
+  },
+  getZoom: state => {
+    return state.zoom;
+  },
+  getCenter: (state, getters, rootState) => {
+    return state.center ? state.center : rootState.geolocation;
   }
 };
 
 export const actions = {
   setAddMode({commit}, value) {
     commit('SET_ADD_MODE', value);
+  },
+  setZoom({commit}, value) {
+    commit('SET_ZOOM', value);
+  },
+  setCenter({commit}, value) {
+    commit('SET_CENTER', value);
   }
 };
 
 export const mutations = {
   SET_ADD_MODE: (state, value) => {
     state.addMode = value;
+  },
+  SET_ZOOM: (state, value) => {
+    state.zoom = value;
+  },
+  SET_CENTER: (state, value) => {
+    state.center = value;
   }
 };
 
