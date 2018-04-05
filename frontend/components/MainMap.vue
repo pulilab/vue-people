fd7133e934076c36eecab1ae2c4f07320a9d8075
lordfuoco <lordfuoco@gmail.com>
2018-04-05 17:13:51 +0200

    feat: add legend, return to home button and the autoCentering functionality on user location 'discover', support for legend hovering on userTypes

--- frontend/components/MainMap.vue
+++ frontend/components/MainMap.vue
@@ -6,6 +6,7 @@
       <no-ssr>
         <l-map
           v-if="center"
+          ref="mainMap"
           :zoom="zoom"
           :options="mapOptions"
           :center="center"
@@ -15,12 +16,32 @@
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"/>
           <l-marker
             v-for="pin in pins"
-            :key="pin.id"
+            :key="pin.key"
+            :options="pin.options"
             :lat-lng="pin.latlng"
-            @click="openPersonDetails(pin)"/>
+            @click="openPersonDetails(pin)"
+          >
+            <l-tooltip>
+              <user-avatar
+                :person="pin"
+              />
+            </l-tooltip>
+          </l-marker>
+
           <l-marker
             v-if="userMaker"
             :lat-lng="userMaker"/>
+
+          <v-btn
+            v-if="showCenterToUser"
+            class="home-button white"
+            icon
+            light
+            @click="centerToUser">
+            <v-icon>gps_fixed</v-icon>
+          </v-btn>
+          <map-legend />
+          <l-control-zoom position="bottomright" />
         </l-map>
       </no-ssr>
     </div>
@@ -28,14 +49,19 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions, mapState} from 'vuex';
 import MapToolbar from './MapToolbar.vue';
+import UserAvatar from './UserAvatar.vue';
+import MapLegend from './MapLegend.vue';
+import { debounce } from '~/utilities/common';
 
 import NoSSR from 'vue-no-ssr';
 export default {
   components: {
     'no-ssr': NoSSR,
-    MapToolbar
+    MapToolbar,
+    UserAvatar,
+    MapLegend
   },
   data () {
     return {
@@ -43,22 +69,47 @@ export default {
     };
   },
   computed: {
+    ...mapState({
+      userLocation: s => {
+        if(s.geolocation){
+          return {lat: s.geolocation.lat, lng: s.geolocation.lng};
+        }
+      }
+    }),
     ...mapGetters({
-      pins: 'people/getList',
+      pins: 'map/getFilteredPins',
       addMode: 'map/isAddMode',
       userPosition: 'user/getUserPosition',
       zoom: 'map/getZoom',
-      center: 'map/getCenter'
+      center: 'map/getCenter',
+      autoCentered: 'map/getAutoCentered'
     }),
     userMaker() {
       return this.userPosition;
+    },
+    showCenterToUser() {
+      return this.userLocation && this.userLocation.lat;
     }
+  },
+  watch: {
+    userLocation: {
+      immediate: true,
+      handler(loc) {
+        this.autoCenter();
+      }
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.$refs.mainMap.mapObject.whenReady(this.autoCenter.bind(this));
+    });
   },
   methods: {
     ...mapActions({
       setUserPosition: 'user/setUserPosition',
       setZoom: 'map/setZoom',
       setCenter: 'map/setCenter',
+      setAutoCentered: 'map/setAutoCentered',
     }),
     addMarker(event) {
       if(this.addMode) {
@@ -68,11 +119,24 @@ export default {
     openPersonDetails(pin) {
       this.$router.push(`/user/${pin.id}/`);
     },
-    mapMoveHandler(e) {
+    mapMoveHandler: debounce(function(e) {
       this.setZoom(e.target.getZoom());
       this.setCenter(e.target.getCenter());
+    }, 200),
+    centerToUser() {
+      if (this.$refs.mainMap && this.userLocation && this.userLocation.lat) {
+        this.$refs.mainMap.mapObject.flyTo(this.userLocation, 13);
+        return true;
+      }
+    },
+    autoCenter() {
+      if(!this.autoCentered) {
+        if (this.centerToUser()) {
+          this.setAutoCentered(true);
+        }
+      }
     }
-  },
+  }
 };
 </script>
 
@@ -89,6 +153,15 @@ export default {
         height: 100%;
         z-index: 2;
     }
+
+    .home-button {
+      position: absolute;
+      z-index: 5000;
+      bottom: 76px;
+      right: 2px;
+    }
+
+
 }
 
 </style>
