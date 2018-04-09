b22124136b791542166160bcbf929519d522ae35
lordfuoco <lordfuoco@gmail.com>
2018-04-09 12:40:09 +0200

    feat: added usertype select, removed github url  as an editable input ( for now ) fixed, colors and added proper toolbar

--- frontend/components/UserProfileForm.vue
+++ frontend/components/UserProfileForm.vue
@@ -1,7 +1,11 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <v-layout row>
+  <div class="user-profile-form">
+    <v-toolbar
+      absolute
+      light>
+      <v-layout
+        class="top-bar"
+        row>
         <v-flex>
           <user-avatar
             :id="userProfile.id"
@@ -11,14 +15,16 @@
           <v-btn
             to="/"
             icon
+            light
             nuxt >
             <v-icon>close</v-icon>
           </v-btn>
         </v-flex>
       </v-layout>
-    </v-card-title>
-    <v-card-text>
-      <form>
+    </v-toolbar>
+
+    <div class="form-container">
+      <form class="form">
         <v-text-field
           v-validate="'required|max:254'"
           v-model="profile.name"
@@ -27,20 +33,35 @@
           label="Name"
           data-vv-name="name"
           required
+          light
         />
+
+        <v-select
+          v-model="profile.type"
+          :items="userTypes"
+          label="Selecte usertype"
+          item-text="name"
+          item-value="id"
+          item-disabled="disabled"
+          light
+        />
+
         <v-text-field
           v-validate="'email'"
           v-model="profile.email"
           :error-messages="errors.collect('email')"
           label="E-mail"
           data-vv-name="email"
+          light
         />
         <v-text-field
           v-validate="'url'"
+          v-if="false"
           v-model="profile.githubUrl"
           :error-messages="errors.collect('githubUrl')"
           label="GitHub profile"
           data-vv-name="githubUrl"
+          light
         />
         <v-text-field
           v-validate="'url'"
@@ -48,6 +69,7 @@
           :error-messages="errors.collect('twitterUrl')"
           label="Twitter profile"
           data-vv-name="twitterUrl"
+          light
         />
         <v-text-field
           v-validate="'url'"
@@ -55,12 +77,14 @@
           :error-messages="errors.collect('websiteUrl')"
           label="Your website"
           data-vv-name="websiteUrl"
+          light
         />
         <v-select
           :items="organisations"
           v-model="profile.organisation"
           label="Organisation"
           autocomplete
+          light
         />
 
         <v-select
@@ -71,6 +95,7 @@
           tags
           chips
           deletable-chips
+          light
         />
 
         <v-text-field
@@ -82,19 +107,26 @@
           name="about"
           label="About"
           textarea
+          light
         />
 
       </form>
-    </v-card-text>
-    <v-card-actions>
+    </div>
+    <v-layout
+      class="actions"
+      align-center
+      row >
+      <div class="last-saved black--text">
+        last saved on:
+      </div>
       <v-spacer />
       <v-btn
         color="primary"
         @click.stop="save">
         Save
       </v-btn>
-    </v-card-actions>
-  </v-card>
+    </v-layout>
+  </div>
 </template>
 
 <script>
@@ -125,7 +157,8 @@ export default {
       userProfile: 'user/getUserProfile',
       usLoggedIn: 'user/getLoginStatus',
       tagList: 'getTags',
-      organisations: 'getOrganizations'
+      organisations: 'getOrganizations',
+      userTypes: 'getUserTypes'
     })
   },
   watch: {
@@ -152,5 +185,35 @@ export default {
   .pull-right {
     display: flex;
     justify-content: flex-end;
+  }
+
+  .user-profile-form {
+    height: 100%;
+
+    .top-bar {
+      padding: 12px;
+    }
+
+    .form-container {
+      height: calc(100% - 80px);
+      position: relative;
+      top: 80px;
+      width: 100%;
+      overflow-y: auto;
+
+      .form {
+        padding: 24px 24px 80px 24px;;
+      }
+    }
+
+     .actions {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 0 12px;
+        background-color: #FFF;
+      }
+
   }
 </style>
