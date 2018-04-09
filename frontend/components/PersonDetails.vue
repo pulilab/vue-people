0d0a2d522382856f15d2873e94f325b4432563b8
lordfuoco <lordfuoco@gmail.com>
2018-04-09 12:40:47 +0200

    fix: refactored person details html for proper scrolling and positioning

--- frontend/components/PersonDetails.vue
+++ frontend/components/PersonDetails.vue
@@ -25,64 +25,66 @@
         :id="person.id"
       />
     </v-toolbar>
-    <div
-      v-if="person"
-      class="person-info">
-      <div class="item">
-        <v-layout row>
-          <v-btn
-            :href="person.githubUrl"
-            icon
-            large
-            target="_blank">
-            <v-icon>mdi-github-circle</v-icon>
-          </v-btn>
-          <v-btn
-            :href="person.twitterUrl"
-            icon
-            large
-            target="_blank">
-            <v-icon>mdi-twitter</v-icon>
-          </v-btn>
-          <v-btn
-            :href="person.websiteUrl"
-            icon
-            large
-            target="_blank">
-            <v-icon>mdi-web</v-icon>
-          </v-btn>
-        </v-layout>
+    <div class="person-info-container">
+      <div
+        v-if="person"
+        class="person-info">
+        <div class="item">
+          <v-layout row>
+            <v-btn
+              :href="person.githubUrl"
+              icon
+              large
+              target="_blank">
+              <v-icon>mdi-github-circle</v-icon>
+            </v-btn>
+            <v-btn
+              :href="person.twitterUrl"
+              icon
+              large
+              target="_blank">
+              <v-icon>mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn
+              :href="person.websiteUrl"
+              icon
+              large
+              target="_blank">
+              <v-icon>mdi-web</v-icon>
+            </v-btn>
+          </v-layout>
+        </div>
+        <div class="item">
+          <div class="title">
+            About
+          </div>
+          <div class="content">
+            {{ person.about }}
+          </div>
+        </div>
+        <div class="item">
+          <div class="title" >
+            Organisation
+          </div>
+          <div class="content">
+            {{ person.organisation }}
+          </div>
+        </div>
+        <div class="item">
+          <div class="title" >
+            Tags
+          </div>
+          <div class="content">
+            <v-chip
+              v-for="(tag, index) in person.tags"
+              :key="index"
+              small>
+              {{ tag }}
+            </v-chip>
+          </div>
+        </div>
+        <repository-list/>
       </div>
-      <div class="item">
-        <div class="title">
-          About
-        </div>
-        <div class="content">
-          {{ person.about }}
-        </div>
-      </div>
-      <div class="item">
-        <div class="title" >
-          Organisation
-        </div>
-        <div class="content">
-          {{ person.organisation }}
-        </div>
-      </div>
-      <div class="item">
-        <div class="title" >
-          Tags
-        </div>
-        <div class="content">
-          <v-chip
-            v-for="(tag, index) in person.tags"
-            :key="index"
-            small>
-            {{ tag }}
-          </v-chip>
-        </div>
-      </div>
-      <repository-list/>
     </div>
   </div>
 </template>
@@ -122,9 +124,17 @@ export default {
   position: relative;
   width: 100%;
 
-  .person-info {
+  .person-info-container {
     position: relative;
-    padding-top: 150px;
+    height: calc(100% - 130px);
+    top: 130px;
+    width: 100%;
+    overflow-y: auto;
+
+    .person-info {
+      position: relative;
+      padding-top: 150px;
+    }
   }
 }
 
