733d7cee210db46f0654e0a01f147cd562510266
lordfuoco <lordfuoco@gmail.com>
2018-04-12 12:06:54 +0200

    feat: move the map from osm to ESRI_Topo_Map

--- frontend/components/MainMap.vue
+++ frontend/components/MainMap.vue
@@ -15,7 +15,7 @@
           @zoomend="mapZoomHandler"
           @click="addMarker">
           <l-tilelayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'"/>
           <l-marker
             v-for="pin in pins"
             :key="pin.key"
