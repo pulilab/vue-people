6b2172fa136e25ba460625d6c19fbd5ad97b3145
Gyorgy Fekete <f@pulilab.com>
2018-04-12 17:47:56 +0200

    include API docs in debug mode

--- django/peoplebackend/urls.py
+++ django/peoplebackend/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from people.views import TestView
+from rest_framework.documentation import include_docs_urls
 
+admin.site.site_header = 'Vue People Backend'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('api/test', TestView.as_view()),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    urlpatterns.append(path(r'api/docs/', include_docs_urls(title='Vue People API', description='Private API')))
