02651b71b7fe9b487112e311c1f521c727393544
Gyorgy Fekete <f@pulilab.com>
2018-04-13 11:06:24 +0200

    usertype views

--- django/people/views.py
+++ django/people/views.py
@@ -1,5 +1,11 @@
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+
+from .serializers import UserTypeSerializer
+from .models import Person, Type
+
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
@@ -7,3 +13,11 @@ class TestView(APIView):
 
     def post(self, request, *args, **kwargs):
         return Response({"success": True})
+
+
+class PersonViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = Person.objects.all()
+
+class UserTypeViewSet(ListModelMixin, GenericViewSet):
+    queryset = Type.objects.all()
+    serializer_class = UserTypeSerializer
