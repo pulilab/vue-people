014065dc1cb6bfd462bdb4c99ab90f2445855a14
Gyorgy Fekete <f@pulilab.com>
2018-06-07 11:12:30 -0500

    public email field that defaults to True

--- django/people/migrations/0011_person_public_email.py
+++ django/people/migrations/0011_person_public_email.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='person',
             name='public_email',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=True),
         ),
     ]
