15aa472ee77097699d2b31859ca34668c0af160f
lordfuoco <lordfuoco@gmail.com>
2018-06-13 16:44:40 +0200

    chore: manually revert auth changes commits

--- django/peoplebackend/settings.py
+++ django/peoplebackend/settings.py
@@ -145,8 +145,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'people.authentication.CsrfExemptSessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication'
     )
 }
 
