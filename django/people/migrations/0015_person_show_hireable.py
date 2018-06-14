24f4914b20939718ffa7d83a6621f4a5d5b80711
Gyorgy Fekete <f@pulilab.com>
2018-06-14 11:46:48 +0200

    fix migration order

--- django/people/migrations/0015_person_show_hireable.py
+++ django/people/migrations/0015_person_show_hireable.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('people', '0013_person_news_opt_in'),
+        ('people', '0014_auto_20180613_1527'),
     ]
 
     operations = [
