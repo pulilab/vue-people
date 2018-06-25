38f87c85352e5fa99a190ce76d49701498b443a3
lordfuoco <lordfuoco@gmail.com>
2018-06-25 13:01:33 +0200

    feat: run new circle repositioning on overlapping markers

--- frontend/store/events.js
+++ frontend/store/events.js
@@ -1,5 +1,6 @@
 import { findGroups } from '../integrations/meetup/signedUrls';
 import { groupParser } from '../integrations/meetup/utilities';
+import { circleOfCoords } from '../utilities/coords';
 
 export const state = () => ({
   meetups: [],
@@ -25,7 +26,27 @@ export const getters = {
 export const actions = {
   async loadMeetups ({commit}) {
     const data = await this.$axios.get(findGroups);
-    commit('SET_MEETUP_LIST', data.data);
+    const meetups = data.data.map(groupParser);
+    const dict = meetups.reduce((p, c, index) => {
+      const key = JSON.stringify(c.latlng);
+      if (p[key]) {
+        p[key].push(index);
+      } else {
+        p[key] = [index];
+      }
+      return p;
+    }, {});
+    for (const k in dict) {
+      if (dict[k].length > 1) {
+        const latlng = JSON.parse(k);
+        const circle = circleOfCoords(latlng, dict[k].length);
+        console.log(circle.length, dict[k].length);
+        dict[k].forEach((value, index) => {
+          meetups[value].latlng = circle[index];
+        });
+      }
+    }
+    commit('SET_MEETUP_LIST', meetups);
   },
   setCurrent ({commit}, id) {
     commit('SET_CURRENT_MEETUP', id);
@@ -34,8 +55,7 @@ export const actions = {
 
 export const mutations = {
   SET_MEETUP_LIST: (state, meetups) => {
-    const parsed = meetups.map(groupParser);
-    state.meetups = parsed;
+    state.meetups = meetups;
   },
   SET_CURRENT_MEETUP: (state, id) => {
     state.currentMeetup = id;
