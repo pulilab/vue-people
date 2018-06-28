dc32269e8d860c8bc8fe1f37aa7c80599e2eeeef
Gyorgy Fekete <f@pulilab.com>
2018-06-28 15:28:21 +0200

    use load_env in wsgi instead of explicit def

--- django/peoplebackend/wsgi.py
+++ django/peoplebackend/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-import dotenv
 from django.core.wsgi import get_wsgi_application
+from . import load_env
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+load_env.load_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peoplebackend.settings")
 
