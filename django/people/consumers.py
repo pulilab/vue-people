b73abe4a5fadc4d727833c5c88bdc6e38b5eb355
lordfuoco <lordfuoco@gmail.com>
2018-07-06 17:40:19 +0200

    feat: consumer to notify websocket of new / updated people

---
+++ django/people/consumers.py
@@ -0,0 +1,22 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
+from people.serializers import PersonSerializer
+from people.models import Person
+
+class PersonConsumer(JsonWebsocketConsumer):
+    activation_code = None
+
+    def connect(self):
+        self.accept()
+        async_to_sync(self.channel_layer.group_add)("people", self.channel_name)
+        self.send_json(content={"handshake": "all fine"})
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)("people", self.channel_name)
+
+    def send_person(self, event):
+        person = Person.objects.get(id=event['id'])
+        p = PersonSerializer(person).data
+        self.send_json({
+            "person": p
+        })
