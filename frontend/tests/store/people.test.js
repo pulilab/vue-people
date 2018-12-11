c4768b6977c0999898451215b20bba9f210115de
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-12-11 11:59:32 +0100

    fix: adjust tests to conform to new store functions

--- frontend/tests/store/people.test.js
+++ frontend/tests/store/people.test.js
@@ -21,22 +21,20 @@ describe('getters', () => {
   });
 
   test('getList', () => {
-    jest.spyOn(parsersUtils, 'latLngParser');
     const getCurrentPerson = 1;
     const rootGetters = {
       'user/getUserProfile': {id: 2}
     };
     s.list = [
-      { id: 1, location: { lat: 2, lng: 3 } },
-      {id: 2, location: { lat: 4, lng: 5 }},
-      {id: 3, type: 3}
+      { id: 1 },
+      { id: 2 },
+      { id: 3 }
     ];
     const result = getters.getList(s, {getCurrentPerson}, {}, rootGetters);
     expect(result).toEqual([
-      {id: 1, selected: true, latlng: { lat: 2, lng: 3 }, type: 1, location: undefined},
-      {id: 3, selected: false, type: 3, latlng: undefined, location: undefined}
+      { id: 1 },
+      { id: 3 }
     ]);
-    expect(parsersUtils.latLngParser).toHaveBeenCalledTimes(2);
   });
 
   test('getLatestUser', () => {
@@ -140,10 +138,11 @@ describe('actions', () => {
 
   test('loadPeople', async () => {
     actions.$axios.get.mockReturnValue({data: [1]});
-    parsersUtils.apiReadParser = jest.fn().mockReturnValue(1);
+    parsersUtils.apiReadParser = jest.fn().mockReturnValue({ a: 1 });
+    parsersUtils.latLngParser = jest.fn().mockReturnValue(1);
     await actions.loadPeople(vuex);
     expect(actions.$axios.get.mock.calls[0]).toEqual(['/api/people/']);
-    expect(vuex.commit.mock.calls[0]).toEqual(['SET_PEOPLE_LIST', [1]]);
+    expect(vuex.commit.mock.calls[0]).toEqual(['SET_PEOPLE_LIST', [{a: 1, latlng: 1, selected: false, type: 1, location: undefined}]]);
   });
 
   test('setCurrent', () => {
