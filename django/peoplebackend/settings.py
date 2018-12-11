d949b21431aa9e0fdb61ad45cf99270270b56330
Gyorgy Fekete <f@pulilab.com>
2018-12-11 14:36:07 +0100

    use new redis cache layer

--- django/peoplebackend/settings.py
+++ django/peoplebackend/settings.py
@@ -222,11 +222,13 @@ LOGIN_REDIRECT_URL = '/user?token={}'
 LOGIN_REDIRECT_URL_DEV = 'http://localhost:3000/user?token={}'
 
 CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': [
-            'redis:6379',
-        ],
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SERIALIZER": "django_redis.serializers.json.JSONSerializer",
+        }
     }
 }
 
