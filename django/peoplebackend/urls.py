d074ffb7f291b3a7bd178df3ab3f5c87627c6d4b
Gyorgy Fekete <f@pulilab.com>
2018-12-13 11:04:04 +0100

    remove debug toolbar from urls

--- django/peoplebackend/urls.py
+++ django/peoplebackend/urls.py
@@ -40,8 +40,6 @@ urlpatterns += [
 ]
 
 if settings.DEBUG:  # pragma: no cover
-    import debug_toolbar
-    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
     urlpatterns.append(path(r'api/docs/', include_docs_urls(title='Vue People API',
                                                             description='Private API',
                                                             authentication_classes=[],
