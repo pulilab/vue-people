a26f0bc76a6e186c664c8addcbd2276f79e51d83
Gyorgy Fekete <f@pulilab.com>
2019-06-20 18:53:18 +0200

    better exception handling so sync doesn't fail on deleted meetup groups

--- django/people/tasks.py
+++ django/people/tasks.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from celery.schedules import crontab
 from celery.task import periodic_task
 from celery.utils.log import get_task_logger
+from requests import HTTPError
 
 from .models import MeetupGroup, MeetupEvent
 
@@ -14,18 +15,21 @@ logger = get_task_logger(__name__)
 
 
 def sync_groups():
-    r = requests.get('{}?radius=global&topic_id={}&key={}'.format(settings.MEETUP_GROUPS_API_URL,
-                                                                  ','.join(settings.MEETUP_TOPIC_IDS),
-                                                                  settings.MEETUP_API_KEY))
-    r.raise_for_status()
-    groups = r.json()
+    try:
+        r = requests.get('{}?radius=global&topic_id={}&key={}'.format(settings.MEETUP_GROUPS_API_URL,
+                                                                      ','.join(settings.MEETUP_TOPIC_IDS),
+                                                                      settings.MEETUP_API_KEY))
+    except HTTPError:
+        pass
+    else:
+        groups = r.json()
 
-    for group in groups:
-        mg, created = MeetupGroup.objects.get_or_create(id=group['id'], defaults=dict(data=group))
-        if not created:
-            if mg.data != group:
-                mg.data = group
-                mg.save()
+        for group in groups:
+            mg, created = MeetupGroup.objects.get_or_create(id=group['id'], defaults=dict(data=group))
+            if not created:
+                if mg.data != group:
+                    mg.data = group
+                    mg.save()
 
 
 def sync_events():
@@ -35,21 +39,25 @@ def sync_events():
     for group in MeetupGroup.objects.all():
         if "next_event" in group.data:
             events_url = settings.MEETUP_EVENTS_API_URL.replace('<urlname>', group.data['urlname'])
-            r = requests.get('{}?status=upcoming&no_later_than={}&key={}'.format(
-                events_url, next_month_same_day.isoformat(), settings.MEETUP_API_KEY))
-            r.raise_for_status()
-            events = r.json()
 
-            for event in events:
-                utc_date = datetime.fromtimestamp(int(event['time']) / 1000, tz=pytz.UTC)
-                me, created = MeetupEvent.objects.get_or_create(id=event['id'],
-                                                                defaults=dict(group=group, date=utc_date, data=event))
-                if not created:
-                    if me.data != event:
-                        me.data = event
-                        me.date = utc_date
-                        me.save()
-            time.sleep(1)  # anti-throttle
+            try:
+                r = requests.get('{}?status=upcoming&no_later_than={}&key={}'.format(
+                    events_url, next_month_same_day.isoformat(), settings.MEETUP_API_KEY))
+            except HTTPError:
+                pass
+            else:
+                events = r.json()
+
+                for event in events:
+                    utc_date = datetime.fromtimestamp(int(event['time']) / 1000, tz=pytz.UTC)
+                    me, created = MeetupEvent.objects.get_or_create(id=event['id'],
+                                                                    defaults=dict(group=group, date=utc_date, data=event))
+                    if not created:
+                        if me.data != event:
+                            me.data = event
+                            me.date = utc_date
+                            me.save()
+                time.sleep(1)  # anti-throttle
 
 
 @periodic_task(run_every=crontab(hour=2, minute=15))
