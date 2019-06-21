5d695270cdebf7ccbe371fe19d72f48f8d53251d
Gyorgy Fekete <f@pulilab.com>
2019-06-21 11:27:08 +0200

    proper sentry exceptiosn

--- django/people/tasks.py
+++ django/people/tasks.py
@@ -9,11 +9,11 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 from celery.utils.log import get_task_logger
 from requests import HTTPError
+from raven.contrib.django.raven_compat.models import client
 
 from .models import MeetupGroup, MeetupEvent
 
 logger = get_task_logger(__name__)
-sentry = logging.getLogger('raven')
 
 def sync_groups():
     try:
@@ -21,16 +21,18 @@ def sync_groups():
                                                                       ','.join(settings.MEETUP_TOPIC_IDS),
                                                                       settings.MEETUP_API_KEY))
     except HTTPError:
-        pass
+        client.captureException(exc_info=True)
     else:
         groups = r.json()
-
-        for group in groups:
-            mg, created = MeetupGroup.objects.get_or_create(id=group['id'], defaults=dict(data=group))
-            if not created:
-                if mg.data != group:
-                    mg.data = group
-                    mg.save()
+        if 'errors' in groups:
+            client.captureMessage('Sync groups error', extra=groups)
+        else:
+            for group in groups:
+                mg, created = MeetupGroup.objects.get_or_create(id=group['id'], defaults=dict(data=group))
+                if not created:
+                    if mg.data != group:
+                        mg.data = group
+                        mg.save()
 
 
 def sync_events():
@@ -45,11 +47,11 @@ def sync_events():
                 r = requests.get('{}?status=upcoming&no_later_than={}&key={}'.format(
                     events_url, next_month_same_day.isoformat(), settings.MEETUP_API_KEY))
             except HTTPError:
-                sentry.error('Sync event HTTP error', exc_info=1)
+                client.captureException(exc_info=True)
             else:
                 events = r.json()
                 if 'errors' in events:
-                    sentry.error('Sync event error', extra=events)
+                    client.captureMessage('Sync event error', extra=events)
                 else:
                     for event in events:
                         utc_date = datetime.fromtimestamp(int(event['time']) / 1000, tz=pytz.UTC)
